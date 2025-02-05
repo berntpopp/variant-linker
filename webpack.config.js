759ba81ae5dfa6a26d1029068500efda554049bd
Bernt Popp <bernt.popp@gmail.com>
2025-02-05 16:42:09 +0100

    feat(build): add Webpack configuration and scripts for building and serving the project; update entry point and dependencies

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  // Set the entry point to the library’s index file.
+  entry: './src/index.js',
+  output: {
+    filename: 'variant-linker.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    // Export as a UMD library so it can be loaded in different module systems.
+    library: 'VariantLinker',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  // Tell webpack what to do with Node built-in modules.
+  resolve: {
+    fallback: {
+      // Disable file system functions because they are not available in the browser.
+      fs: false,
+      // If you need to use "path" (for example, if any module requires it),
+      // you can supply a browser-compatible version.
+      path: require.resolve('path-browserify'),
+      // You may add others as needed (for example, os, crypto, etc.):
+      // os: false,
+      // crypto: require.resolve('crypto-browserify'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        // Transpile our source code with Babel
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        // JSON files can be loaded natively in webpack 5 (or use json-loader if needed)
+        test: /\.json$/,
+        type: 'javascript/auto',
+        use: [ 'json-loader' ]
+      }
+    ]
+  },
+  // You may also need to set mode to 'production' or 'development'
+  mode: 'development'
+};
